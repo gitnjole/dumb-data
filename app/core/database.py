881beefcf22a6e8bf48414accce8e0725dd590b1
Antonio Cahun <cahunantonio1@gmail.com>
2025-05-16 12:50:45 +0200

    feat: add database connection

---
+++ app/core/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+import os
+
+DATABASE_URL = os.getennv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autoflush=False, bind=engine)
+Base = declarative_base()
