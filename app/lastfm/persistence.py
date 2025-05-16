799f7b5d08b00e9241825977b81073aee97f7426
Antonio Cahun <cahunantonio1@gmail.com>
2025-05-16 12:51:46 +0200

    feat: implement basic reader/writer

--- app/lastfm/persistence.py
+++ app/lastfm/persistence.py
@@ -1,1 +1,17 @@
-## TODO Implement persistence
+from app.core.database import SessionLocal
+from app.lastfm.models.sql_models import ScrobbleORM
+from app.models.lastfm_models import Scrobble
+
+class LastFMPersistence:
+    def __init__(self):
+        self.db = SessionLocal()
+
+    def save_scrobble(self, scrobble: Scrobble):
+        db_obj = ScrobbleORM(**scrobble.model_dump())
+        self.db.add(db_obj)
+        self.db.commit()
+        self.db.refresh(db_obj)
+        return db_obj
+    
+    def list_scrobbles(self):
+        return self.db.query(ScrobbleORM).all()
