68de31f78fe263ffff01f5ddbc5f8c4c69ed5c24
Antonio Cahun <cahunantonio1@gmail.com>
2025-05-21 15:53:14 +0200

    feat: config alembic

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,11 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from app.core.database import Base
+from app.lastfm.models.sql_models import ScrobbleORM
+
+import os
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -14,11 +19,13 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+config.set_main_option('sqlalchemy.url', os.environ["DATABASE_URL"])
+
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
