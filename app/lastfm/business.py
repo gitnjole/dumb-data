2c96fe8d286dd13c7e3275715df0b772740ea685
Antonio Cahun <cahunantonio1@gmail.com>
2025-05-22 23:30:35 +0200

    refactor: update usage of business layer

--- app/lastfm/business.py
+++ app/lastfm/business.py
@@ -1,21 +1,35 @@
 from app.lastfm.client import LastFMClient
-from app.models.lastfm_models import ScrobbleTransfer
+from app.models.lastfm_models import ScrobbleTransfer, ScrobbleCollectionTransfer
+from app.lastfm.persistence.writer import ScrobbleWriter
+from app.lastfm.persistence.reader import ScrobbleReader
 
+from app.core.datetime_utils import parse_playback_date
 
 class LastFMBusiness:
     def __init__(self, client: LastFMClient, username: str):
         self.client = client
         self.username = username
+        self.writer = ScrobbleWriter()
+        self.reader = ScrobbleReader()
 
-    def fetch_scrobbles(self, limit: int = None)-> ScrobbleTransfer:
+    def fetch_scrobbles(self, limit: int = None)-> ScrobbleCollectionTransfer:
         raw_tracks = self.client.get_recent_tracks(self.username, limit=limit)
 
-        return [
-            ScrobbleTransfer(
-                track=played.track.title,
-                artist=played.track.artist.name,
-                album=played.album if played.album else None,
-                playback_date=played.playback_date,
-            )
-            for played in raw_tracks
-        ]
+        collection = ScrobbleCollectionTransfer(
+            scrobbles = [
+                ScrobbleTransfer(
+                    track=played.track.title,
+                    artist=played.track.artist.name,
+                    album=played.album if played.album else None,
+                    playback_date=parse_playback_date(played.playback_date),
+                )
+                for played in raw_tracks
+            ]
+        )
+
+        self._persist_scrobbles(collection)
+
+        return collection
+    
+    def _persist_scrobbles(self, scrobbles: ScrobbleCollectionTransfer):
+        self.writer.save_collection(scrobbles)
