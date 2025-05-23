294be644b0cacfa8a48e85155ebea5f57636cf59
Antonio Cahun <cahunantonio1@gmail.com>
2025-05-23 11:11:49 +0200

    feat: add helper method for timestamp insertion

--- app/models/lastfm_models.py
+++ app/models/lastfm_models.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, field_serializer
 from typing import Optional
 from datetime import datetime
+from app.lastfm.models.sql_models import ScrobbleORM
 
 from app.core.datetime_utils import LASTFM_DATETIME_FORMAT
 
@@ -14,6 +15,11 @@ class ScrobbleTransfer(BaseModel):
     @field_serializer('playback_date')
     def serialize_date(self, dt: datetime) -> str:
         return dt.strftime(LASTFM_DATETIME_FORMAT)
+    
+    def insert_timestamp(self, timestamp: Optional[datetime] = None) -> ScrobbleORM:
+        data = self.model_dump()
+        data["timestamp"] = timestamp or datetime.now()
+        return ScrobbleORM(**data)
 
 class ScrobbleCollectionTransfer(BaseModel):
     scrobbles: list[ScrobbleTransfer]
